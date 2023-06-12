dc3d38aa12e30f13285a6c07771e21235b94f6a6
mrsuitz <duke12799@gmail.com>
2023-06-12 13:17:35 -0400

    added api.js file

---
+++ api.js
@@ -0,0 +1,49 @@
+const fetchAllPlayers = async () => {
+    try {
+      const response = await fetch(APIURL + 'players');
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error('Uh oh, trouble fetching players!', err);
+    }
+  };
+  
+  const fetchSinglePlayer = async (playerId) => {
+    try {
+      const response = await fetch(APIURL + `players/${playerId}`);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error(`Oh no, trouble fetching player #${playerId}!`, err);
+    }
+  };
+  
+  const addNewPlayer = async (playerObj) => {
+    try {
+      const response = await fetch(APIURL + 'players', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(playerObj)
+      });
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error('Oops, something went wrong with adding that player!', err);
+    }
+  };
+  
+  const removePlayer = async (playerId) => {
+    try {
+      const response = await fetch(APIURL + `players/${playerId}`, {
+        method: 'DELETE'
+      });
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.error(`Whoops, trouble removing player #${playerId} from the roster!`, err);
+    }
+  };
+  
+  export { fetchAllPlayers, fetchSinglePlayer, addNewPlayer, removePlayer };
